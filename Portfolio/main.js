b805720950f39f4555559119d61fb180532e9c34
ramananbalu <ramananbalu3032@gmail.com>
2024-10-18 14:00:43 +0530

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".text", {
+    strings:["Frontend Developer","Backend Developer", "Gamer" , "Fullstack Developer"],
+    typedSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
